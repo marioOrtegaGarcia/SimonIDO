319edb2526408134d738feba2407fd4051bde1bb
marioOrtegaGarcia <mario.j.ortega.garcia@gmail.com>
2020-05-07 19:49:35 -0700

    Cleaned up 2 files minor

--- flask/project/__init__.py
+++ flask/project/__init__.py
@@ -13,9 +13,9 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
-app.debug = True
 from werkzeug.debug import DebuggedApplication
 app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+app.debug = True
 
 app.secret_key = os.environ['APP_SECRET_KEY']
 bs = Bootstrap(app)
